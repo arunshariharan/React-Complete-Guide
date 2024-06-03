380c888ee16fe0ee9f4cf789d2fc0143aaebba68
Arun <arunshariharan@gmail.com>
2024-06-04 00:17:47 +1000

    Add an onClick listener

--- 3-ReactEssentials/src/Components/TabButton/TabButton.jsx
+++ 3-ReactEssentials/src/Components/TabButton/TabButton.jsx
@@ -1,9 +1,13 @@
 import { Children } from "react";
 
 export default function TabButton({children}) {
+  const handleClick = () => {
+    console.log("Clicked");
+  }
+  
   return (
     <li>
-      <button>
+      <button onClick={handleClick}>
         {children}
       </button>
     </li>
