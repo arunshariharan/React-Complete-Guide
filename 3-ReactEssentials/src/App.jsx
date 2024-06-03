9be6780d5cd87e191baca5588f79abf523a3f4c4
Arun <arunshariharan@gmail.com>
2024-06-03 21:42:42 +1000

    Add props to dynamically render data

--- 3-ReactEssentials/src/App.jsx
+++ 3-ReactEssentials/src/App.jsx
@@ -1,4 +1,5 @@
 import reactImg from "./assets/react-core-concepts.png";
+import componentsImg from "./assets/components.png";
 
 const reactDescription = ["Fundamental", "Crucial", "Core"];
 
@@ -13,10 +14,20 @@ function Header() {
       <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        {descriptor} React concepts you will need for
-        almost any app you are going to build!
+        {descriptor} React concepts you will need for almost any app you are
+        going to build!
       </p>
     </header>
+  );
+}
+
+function CoreConcepts(props) {
+  return (
+    <li>
+      <img src={props.imgSrc} alt={props.title} />
+      <h2>{props.title}</h2>
+      <h3>{props.description}</h3>
+    </li>
   );
 }
 
@@ -26,6 +37,23 @@ function App() {
       <Header />
       <main>
         <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <ul>
+            <CoreConcepts
+              title="Concept 1"
+              description="Main Concept"
+              imgSrc={componentsImg}
+            />
+            <CoreConcepts
+              title="Concept 2"
+              description="Main Concept 2"
+              imgSrc={componentsImg}
+            />
+
+            <CoreConcepts />
+            <CoreConcepts />
+          </ul>
+        </section>
       </main>
     </div>
   );
