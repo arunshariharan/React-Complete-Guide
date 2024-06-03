4b354fb856da06030a0eeb76322e694820df57c6
Arun Hariharan Sivasubramaniyan <arun.hariharan@xero.com>
2024-06-03 19:33:32 +1000

    Javascript refresher

--- JavascriptRefresher/assets/scripts/app.js
+++ JavascriptRefresher/assets/scripts/app.js
@@ -14,3 +14,83 @@ import * as util from "./util";
 console.log(util.default);
 console.log(util.abc);
 console.log(util.test);
+
+// arrow functions with multiple params
+let concatenateWords = (a,b) => {
+  return a + b;
+}
+console.log(concatenateWords(util.default, util.abc)); // testApiKeyabc
+
+// arrow functions with a single param
+let doubledValue = inputNumber => {
+  return inputNumber * 2;
+}
+console.log(doubledValue(2)); // 4
+
+// arrow functions with a single param and single return line can be written as below 
+let doubledValue2 = inputNumber => inputNumber * 2;
+console.log(doubledValue2(2)); // 4
+
+/* Special case: Just returning an object
+If you go for the shorter alternative explained and you're trying to return a JavaScript object, you may end up with the following, invalid code:
+
+    number => { age: number }; // error trying to return an object
+
+This code would be invalid because JavaScript treats the curly braces as function body wrappers (not as code that creates a JS object).
+
+To "tell" JavaScript that an object should be created (and returned) instead, the code would need to be adjusted like this:
+
+    number => ({ age: number }); // wrapping the object in extra parentheses
+*/
+
+let someObject = () => ({age: 23});
+console.log(someObject());  // Object { age: 23 }
+
+//==================================================================================================================
+//==================================================================================================================
+//==================================================================================================================
+
+console.log(util.abc);
+console.log(util.default);
+console.log(util.testFunction());
+console.log(util.singleLineFunction("test"));
+console.log(util.user);
+console.log(util.user.greet());
+
+// ----------------------------------------------------------------------------
+// Objects
+let user = new util.User("Emma", 23);
+console.log(user.greet());
+
+// Arrays
+console.log(util.hobbies[0]);
+util.hobbies.forEach((hobby) => console.log(hobby));
+
+util.hobbies.map((item) => {
+  item.includes("e")
+    ? console.log(item + " contain the letter e")
+    : console.log(item + " does not contain the letter 'e'");
+});
+
+console.log(util.hobbyRatings);
+
+// ----------------------------------------------------------------------------
+// Destructuring
+console.log(util.firstNameOldWay);
+console.log(util.lastNameOldWay);
+
+console.log(util.firstNameDes);
+console.log(util.lastNameDes);
+
+console.log(util.userObj.name);
+console.log(util.userObj.age);
+
+console.log(util.destructuredName);
+console.log(util.userAge);
+
+util.storeOrder(util.order);
+util.storeOrderDestructred(util.newOrder);
+util.storeOrderDestructred({ id: 3, currency: "INR", amount: 2499.99 });
+
+// ----------------------------------------------------------------------------
+
--- JavascriptRefresher/assets/scripts/util.js
+++ JavascriptRefresher/assets/scripts/util.js
@@ -10,3 +10,104 @@ export let abc = "abc";
 export let test = "test";
 
 // It is possible to export functions too, which is what we mostly do in React
+
+//---------------------------------------------------------------------
+//---------------------------------------------------------------------
+// --------------------------------------------------------------------
+
+// functions
+const testFunction = () => {
+  return "testValue";
+};
+
+export { testFunction };
+
+export let singleLineFunction = (userName) => "Hello " + userName;
+
+// ----------------------------------------------------------------------------
+// objects
+export const user = {
+  name: "Emmanuel",
+  age: 34,
+  greet() {
+    return "Hello " + this.name;
+  },
+};
+
+// ----------------------------------------------------------------------------
+// classes (rarely used)
+export class User {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  greet = () => {
+    return "Hello " + this.name;
+  };
+}
+
+// ----------------------------------------------------------------------------
+// Arrays and array functions
+export let hobbies = ["Cricket", "Football"];
+hobbies.push("Reading");
+export const hobbyRatings = hobbies.map(
+  (hobby) => hobby + ": Rating = " + Math.floor(Math.random() * 10, 2)
+);
+
+// ----------------------------------------------------------------------------
+// Destructuring - array
+// Old way
+const userArray = ["Arun", "S"];
+export const firstNameOldWay = userArray[0];
+export const lastNameOldWay = userArray[1];
+
+// The above can be destructured as follows
+export const [firstNameDes, lastNameDes] = ["Arun", "S"];
+
+// ---------------------------------------------
+// Destructuring Object
+export const userObj = {
+  name: "Arun S",
+  age: 32,
+};
+
+// The above can be destructured as follows.
+// Thing to keep in mind - the destructred name should be the same as the key name.
+// We can also assign alias if we want
+export const { destructuredName, destructuredAge: userAge } = {
+  destructuredName: "Arun S Hariharan",
+  destructuredAge: 33,
+};
+
+// ---------------------------------------------
+// Destructuring - function params
+// Consider the following function and function call.
+export function storeOrder(order) {
+  localStorage.setItem("id", order.id);
+  localStorage.setItem("currency", order.currency);
+
+  console.log(localStorage.getItem("id"));
+  console.log(localStorage.getItem("currency"));
+}
+
+export let order = { id: 1, currency: "USD", amount: 54.99 };
+storeOrder(order);
+
+// In the above function, the param "order" can be destructured as follows
+export function storeOrderDestructred({ id, currency, amount }) {
+  localStorage.setItem("id", id);
+  localStorage.setItem("currency", currency);
+  localStorage.setItem("amount", amount);
+
+  console.log(localStorage.getItem("id"));
+  console.log(localStorage.getItem("currency"));
+  console.log(localStorage.getItem("amount"));
+}
+
+export let newOrder = { id: 2, currency: "AUD", amount: 45.99 };
+storeOrderDestructred(newOrder);
+// can also be called as below
+storeOrderDestructred({ id: 3, currency: "INR", amount: 2499.99 });
+
+// ----------------------------------------------------------------------------
