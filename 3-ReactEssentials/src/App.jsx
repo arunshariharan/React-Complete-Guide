3eddbb2eaecb6db545301e991ea3f9b74404222c
Arun <arunshariharan@gmail.com>
2024-06-03 23:30:14 +1000

    Restructure files into components

--- 3-ReactEssentials/src/App.jsx
+++ 3-ReactEssentials/src/App.jsx
@@ -1,37 +1,6 @@
-import reactImg from "./assets/react-core-concepts.png";
 import { CORE_CONCEPTS } from "./data";
-
-const reactDescription = ["Fundamental", "Crucial", "Core"];
-
-const getRandomInt = (max) => {
-  return Math.floor(Math.random() * (max + 1));
-};
-
-function Header() {
-  const descriptor = reactDescription[getRandomInt(2)];
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {descriptor} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
-// Here the function params are destructred to take in only required params
-// Alternatively, you could just get "props" and then do props.image etc inside the function
-function CoreConcepts({ image, title, description }) {
-  return (
-    <li>
-      <img src={image} alt={title} />
-      <h2>{title}</h2>
-      <h3>{description}</h3>
-    </li>
-  );
-}
+import Header from "./Components/Header";
+import CoreConcepts from "./Components/CoreConcepts";
 
 function App() {
   return (
---
+++ 3-ReactEssentials/src/Components/CoreConcepts.jsx
@@ -0,0 +1,11 @@
+// Here the function params are destructred to take in only required params
+// Alternatively, you could just get "props" and then do props.image etc inside the function
+export default function CoreConcepts({ image, title, description }) {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h2>{title}</h2>
+      <h3>{description}</h3>
+    </li>
+  );
+}
