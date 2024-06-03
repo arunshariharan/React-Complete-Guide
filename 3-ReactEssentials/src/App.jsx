db3fb636e9b421372c6603cd4f08a2b1bd276957
Arun Hariharan Sivasubramaniyan <arun.hariharan@xero.com>
2024-06-03 21:18:21 +1000

    Add dynamic description

--- 3-ReactEssentials/src/App.jsx
+++ 3-ReactEssentials/src/App.jsx
@@ -1,11 +1,18 @@
+const reactDescription = ["Fundamental", "Crucial", "Core"];
+
+const getRandomInt = (max) => {
+  return Math.floor(Math.random() * (max + 1));
+};
+
 function Header() {
+  const descriptor = reactDescription[getRandomInt(2)];
   return (
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
-        going to build!
+        {descriptor} React concepts you will need for
+        almost any app you are going to build!
       </p>
     </header>
   );
