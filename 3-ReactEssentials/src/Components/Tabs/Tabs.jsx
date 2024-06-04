eeaf626786bd73d93848591b312ce2dafe8aedf5
Arun <arunshariharan@gmail.com>
2024-06-04 21:23:30 +1000

    Add Section component, but props such as id are not auto forwarded

---
+++ 3-ReactEssentials/src/Components/Tabs/Tabs.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import TabButton from "../TabButton/TabButton";
+import TabContent from "../TabContent/TabContent";
+import { EXAMPLES } from "../../data";
+import Section from "../Section/Section";
+
+const topics = ["components", "jsx", "props", "state"];
+export default function Tabs() {
+  const [selectedTopic, setSelectedTopic] = useState(
+    <TabContent {...EXAMPLES[topics[0]]} />
+  );
+
+  const [selectedTab, setSelectedTab] = useState(topics[0]);
+
+  const handleSelect = (topic) => {
+    setSelectedTopic(<TabContent {...EXAMPLES[topic]} />);
+    setSelectedTab(topic);
+  };
+
+  return (
+    <>
+      <Section title="Examples" id="examples"> 
+        <menu>
+          {topics.map((topic) => (
+            <TabButton
+              isSelected={selectedTab == topic}
+              onSelect={() => handleSelect(topic)}
+            >
+              {topic.toLocaleUpperCase()}
+            </TabButton>
+          ))}
+        </menu>
+      </Section>
+
+      <Section>
+        {selectedTopic}
+      </Section>
+    </>
+  );
+}
